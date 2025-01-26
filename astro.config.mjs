6037465e53daae1475ce0beb859976679b19fed9
ManuelGil <atencion.ms@outlook.com>
2025-01-25 21:42:33 -0500

    ci: :green_heart: configure site settings and add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
+  site: 'https://imgil.dev',
+  base: '/',
   integrations: [tailwind()],
 })
